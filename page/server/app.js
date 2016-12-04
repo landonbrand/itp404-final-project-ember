213d640009282e4ab9712ef700202c17d67f3799
landonbrand <mail@landonbrand.com>
2016-12-04 13:54:08 -0800

    idk what's happening

--- page/server/app.js
+++ page/server/app.js
@@ -92,7 +92,7 @@ app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/failure' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.send(JSON.parse(req.user));
+    res.send(req.user);
   });
 
 app.get("/page", function(req, res){
