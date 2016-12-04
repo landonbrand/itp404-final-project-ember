61efa90884cbc0407f6fcd6befe9f3bf879f0342
landonbrand <mail@landonbrand.com>
2016-12-04 10:41:38 -0800

    simplified api for testing

--- page/server/app.js
+++ page/server/app.js
@@ -55,7 +55,14 @@ passport.use(new GitHubStrategy({
     //   console.log("findOrCreate running!");
     //   return done(err, user);
     // });
-    done();
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
   }
 ));
 
