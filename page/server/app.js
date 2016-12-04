2b75a1aa3f3e977ad864f8e90bef8a5eb1bab2df
landonbrand <mail@landonbrand.com>
2016-12-04 10:27:57 -0800

    changed callback url for auth

--- page/server/app.js
+++ page/server/app.js
@@ -46,7 +46,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GitHubStrategy({
     clientID: "90c810f2ae9f6b8c1a9e",
     clientSecret: "56acd69b369c551e85a624fd9b48249ff9bbfc85",
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    callbackURL: "http://192.241.235.59:1111/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     console.log("passport.use running");
