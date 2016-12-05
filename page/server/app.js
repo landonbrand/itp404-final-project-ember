b127f8ebc43f91b7ebc65b0c9afbf9277e1944e7
landonbrand <mail@landonbrand.com>
2016-12-04 22:29:19 -0800

    hope

--- page/server/app.js
+++ page/server/app.js
@@ -13,6 +13,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("we're connected to the database!");
+  console.log(db);
 });
 
 var pageSchema = new mongoose.Schema({
